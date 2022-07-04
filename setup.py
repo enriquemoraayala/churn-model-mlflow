391ffadad922c0d14b358a0eb3f3874f6e4d2544
Enrique Mora <enrique.mora@es.nestle.com>
2022-07-04 15:17:52 +0200

    addin cookiecutter template, first push

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='test project to test mlflow',
+    author='Enrique Mora',
+    license='MIT',
+)
